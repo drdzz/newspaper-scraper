d39d03e5c91b36ed52ecaf5afe1853957f8c8af5
Marc Ponce Fernández <marc.ponce@admins-Air.home>
2023-11-10 16:21:29 -0800

    Cleaninf functions

--- lavanguardiascraper.py
+++ lavanguardiascraper.py
@@ -6,15 +6,15 @@ import json
 
 # TODO
 # subtituloslista
-# Poner que pille <h2 y h3> de enmedio de la noticia (quotes etc...) (parece que ya esta si hay mas tipos de textos entonces no)
 # Poner mas tipos de noticias solo soporta una, no hay videos ni noticias en directo.
 # Poner que scarpee todas las noticias y escoja las que mas me gusten
 #     una vez eso funcione poner que salgan todas a la vez en la seccion de noticias de obsidiam, mouseover...etc
 #     que salgan en distintas columans etc.
 # Falta lo de procesar los textos por algun tipo de IA
 
+
 # Arguments
-url = "https://www.lavanguardia.com/local/barcelona/20231110/9366426/35-locales-barcelona-compro-2021-abrir-tiendas-aun-cerrados.html"
+url = "https://www.lavanguardia.com/politica/20231111/9369432/sanchez-alcanza-mayoria-absoluta-transversal-avalar-reeleccion.html"
 
 # GET Request
 def httpGet(url):
@@ -27,18 +27,8 @@ def crearSopa(html):
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-
-###################################### NEW WAY (more general) ################################
 # This suplanta crear Textos /imagenes /videos por separado
-"""def articleModules(soup):
-    titulo = soup.find('h1')
-    titulo = "## " + titulo.text
-    subtitulo = soup.find('h2', class_='epigraph')  # subtituloslista : mira aqui por si hay mas de un subtit debajo el titulo
-    subtitulo = "- " + f"**{subtitulo.text}**" + "\n"
-    article = soup.find('div', class_='article-modules')
-    return titulo, subtitulo, article"""
-
-def extractArticle(article):
+def articleModules(soup):
     titulo = soup.find('h1')
     titulo = "## " + titulo.text
     subtitulo = soup.find('h2', class_='epigraph')  # subtituloslista : mira aqui por si hay mas de un subtit debajo el titulo
@@ -47,6 +37,10 @@ def extractArticle(article):
     foto = soup.find('img', {'data-full-src': lambda x: x and 'lavanguardia' in x})
     foto_portada = f"![Image]({foto.get('data-full-src')})\n"
     foto_pie =  f"*{foto.get('alt')}*\n"
+    return titulo, subtitulo, foto_portada, foto_pie, article
+
+#
+def extractArticle(article):
     extracted_elements = []
     for element in article.find_all():
         if element.name in ['p', 'img', 'h3']:
@@ -65,13 +59,10 @@ def extractArticle(article):
         if isinstance(extracted_elements[i],str):
             elements.append(extracted_elements[i])        
 
-    return titulo, foto_portada, foto_pie, subtitulo, elements
+    return elements
     
-##############################################################################################
-
 # Lista de Noticia
-#def crearNoticia(titulo,subtitulo,texts,image_links): # Se cambia por extracted_elements
-def crearNoticia(titulo,foto_portada,foto_pie,subtitulo,extracted_elements):
+def crearNoticia(titulo,subtitulo,foto_portada,foto_pie,extracted_elements):
     noticia = []
     noticia.append(titulo)
     noticia.append("\n---------------\n")
@@ -79,22 +70,26 @@ def crearNoticia(titulo,foto_portada,foto_pie,subtitulo,extracted_elements):
     noticia.append("---------------\n")
     noticia.append(foto_portada)
     noticia.append(foto_pie+"\n\n\n")
-    #noticia.append("---------------\n")
     for data in extracted_elements:
         noticia.append(data)
     return noticia
 
 # Escribe noticia
 def escribirNoticia(noticia):
-    with open('C:\\Users\\marc.ponce\\Documents\\Obsidian Vault\\noticia.md','w',encoding='utf-8') as file:
+    #with open('C:\\Users\\marc.ponce\\Documents\\Obsidian Vault\\noticia.md','w',encoding='utf-8') as file:     # For Windows
+    with open ('/Users/marc.ponce/Documents/Obsidian Vault/noticia.md','w',encoding='utf-8') as file:      #  For MacOS
         for linea in noticia:
-            #print("??",linea) #optional
             file.write(linea)
     return
 
 # Noticia Normal
 def noticiaNormal(url):
-
+    html = httpGet(url)
+    soup = crearSopa(html)
+    modules = articleModules(soup)
+    article = extractArticle(modules[4])
+    noticia = crearNoticia(modules[0],modules[1],modules[2],modules[3],article)
+    escribirNoticia(noticia)
     return noticia
 
 # Noticia Video
@@ -107,9 +102,4 @@ def noticiaOpinion(url):
 
 
 
-html = httpGet(url)
-soup = crearSopa(html)
-article = extractArticle(soup)
-#print(article)
-noticia = crearNoticia(article[0],article[1],article[2],article[3],article[4])
-escribirNoticia(noticia)
+
